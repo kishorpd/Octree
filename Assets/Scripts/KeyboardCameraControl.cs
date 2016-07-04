81a927db82ca9cb16484c63e79511984a5056f18
Kishor Deshmukh <kdeshmukh@fiea.local>
2016-07-04 11:34:29 -0400

    Tried native library using C++.

---
+++ Assets/Scripts/KeyboardCameraControl.cs
@@ -0,0 +1,88 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO;
+using System;
+
+using System.Collections.Generic;
+
+//[AddComponentMenu("Camera-Control/Keyboard")]
+public class KeyboardCameraControl : MonoBehaviour
+{
+	static float lookSpeed = 5.0f;
+	float moveSpeed = 0.2f;
+
+
+	public struct myVec3
+	{
+		public float _X,_Y, _Z;
+
+		public myVec3(Vector3 vec)
+		{
+			this._X = vec.x;
+			this._Y = vec.y;
+			this._Z = vec.z;
+		}
+		// Override the ToString method:
+		public override string ToString()
+		{
+			return (String.Format("({0},{1},{2})", _X, _Y, _Z));
+		}
+	};
+
+	float rotationX = 0.0f;
+	float rotationY = 0.0f;
+
+	[DllImport("OctreeT", EntryPoint = "TestDivide")]
+	public static extern float StraightFromDllTestDivide(float a, float b);
+
+	[DllImport("OctreeT", EntryPoint = "TestGameObj")]
+	public static extern float TestGameObj(System.IntPtr aa);
+	
+	[DllImport("OctreeT", EntryPoint = "AddOne")]
+	public static extern myVec3 AddOne(myVec3 vec);
+
+	[DllImport("OctreeT", EntryPoint = "stayPersistant")]
+	public static extern int stayPersistant;
+
+	
+	
+	//============================
+	System.IntPtr pnt = Marshal.AllocHGlobal(4);
+                  
+	void Start()
+	{
+		//pnt = (System.IntPtr)4.5f;
+		float straightFromDllDivideResult = StraightFromDllTestDivide(20, 5);
+		// Print it out to the console
+		Debug.Log("First output from dll:" + straightFromDllDivideResult);
+		Vector3 ab = new Vector3(2,3,4);
+		myVec3 mv = new myVec3(ab);
+		Debug.Log("AddOne:" + AddOne(mv));
+		Debug.Log("stayPersistant:" + stayPersistant++);
+	//	Debug.Log("Null or not:" + TestGameObj(pnt));
+		Debug.Log("stayPersistant:" + stayPersistant++);
+
+	}
+
+	void Update()
+	{
+		rotationX += Input.GetAxis("Mouse X") * lookSpeed;
+		rotationY += Input.GetAxis("Mouse Y") * lookSpeed;
+		rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+		transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+		transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+		transform.position += transform.forward * moveSpeed * Input.GetAxis("Vertical");
+		transform.position += transform.right * moveSpeed * Input.GetAxis("Horizontal");
+	}
+
+
+	//myVec3 vectorToMyVec3(Vector3 vec)
+	//{ 
+	//	myVec3 loc = new myVec3();
+	//	loc._X = vec.x;
+	//}
+}
---
+++ Assets/Scripts/gridOverlay.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class gridOverlay : MonoBehaviour
+{
+	
+	// x ht, y , z
+	const float Center = 0.0f;
+	const float MaxDimension = 17.0f;
+	Vector3 CubeDimension = new Vector3(MaxDimension/2,MaxDimension/2,MaxDimension/2);
+
+	
+	private Material lineMaterial;
+
+	private Color mainColor = new Color(0f, 1f, 0f, 1f);
+	private Color subColor = new Color(0f, 0.5f, 0f, 1f);
+
+	public Vector3 PartitionerCenter;
+	public Vector3 CubeWidth;
+	int i = 0;
+	bool completed = true;
+
+	void Start()
+	{
+		PartitionerCenter = new Vector3(Center,Center,Center);
+		CubeWidth = CubeDimension;
+	}
+
+
+	void DrawLine(Vector3 center, Vector3 width)
+	{
+		GL.Vertex(center + width);
+		GL.Vertex(center - width);
+	}
+
+	void DrawPlaneXZ(Vector3 center, Vector3 width)
+	{
+		Vector3 tempWidth = new Vector3();
+		tempWidth.z += width.z;
+		center.x += width.x;
+		DrawLine(center, tempWidth);
+		center.x -= 2 * width.x;
+		DrawLine(center, tempWidth);
+
+		tempWidth.z = 0;
+		center.x += width.x;
+
+
+		tempWidth.x += width.x;
+		center.z += width.z;
+		DrawLine(center, tempWidth);
+		center.z -= 2 * width.z;
+		DrawLine(center, tempWidth);
+
+		tempWidth.x = 0;
+
+	}
+
+	void DrawPlaneXY(Vector3 center, Vector3 width)
+	{
+		Vector3 tempWidth = new Vector3();
+		tempWidth.y += width.y;
+		center.x += width.x;
+		DrawLine(center, tempWidth);
+		center.x -= 2 * width.x;
+		DrawLine(center, tempWidth);
+		tempWidth.y = 0;
+		center.x += width.x;
+
+
+		tempWidth.x += width.x;
+		center.y += width.y;
+		DrawLine(center, tempWidth);
+		center.y -= 2 * width.y;
+		DrawLine(center, tempWidth);
+	}
+
+	void DrawPlaneYZ(Vector3 center, Vector3 width)
+	{
+		Vector3 tempWidth = new Vector3();
+		tempWidth.y += width.y;
+		center.z += width.z;
+		DrawLine(center, tempWidth);
+		center.z -= 2 * width.z;
+		DrawLine(center, tempWidth);
+		tempWidth.y = 0;
+		center.z += width.z;
+
+
+		tempWidth.z += width.z;
+		center.y += width.y;
+		DrawLine(center, tempWidth);
+		center.y -= 2 * width.y;
+		DrawLine(center, tempWidth);
+	}
+
+	void DrawPlanePartitions(Vector3 center, Vector3 width)
+	{
+		DrawPlaneXZ(center, width);
+		DrawPlaneXY(center, width);
+		DrawPlaneYZ(center, width);
+	}
+
+	void DrawCube(Vector3 center, Vector3 width)
+	{
+
+		Vector3 tempCenter = new Vector3();
+		tempCenter = center;
+		tempCenter.y += width.y;
+		DrawPlaneXZ(tempCenter, width);
+		tempCenter.y -= 2*width.y;
+		DrawPlaneXZ(tempCenter, width);
+		tempCenter.y = center.y;
+
+		tempCenter.z += width.z;
+		DrawPlaneXY(tempCenter, width);
+		tempCenter.z -= 2 * width.z;
+		DrawPlaneXY(tempCenter, width);
+		tempCenter.z = center.z;
+		
+		tempCenter.x += width.x;
+		DrawPlaneYZ(tempCenter, width);
+		tempCenter.x -= 2 * width.x;
+		DrawPlaneYZ(tempCenter, width);
+	}
+
+	void DrawPartitioners(Vector3 center, Vector3 width)
+	{
+		DrawLine(center, new Vector3(0, width.y, 0));
+		DrawLine(center, new Vector3(0, 0, width.z));
+		DrawLine(center, new Vector3(width.x, 0, 0));
+		DrawPlanePartitions(center, width);
+	}
+
+	void Update()
+	{
+	}
+
+	void CreateLineMaterial()
+	{
+
+		if (!lineMaterial)
+		{
+			lineMaterial = new Material("Shader \"Lines/Colored Blended\" {" +
+				"SubShader { Pass { " +
+				"    Blend SrcAlpha OneMinusSrcAlpha " +
+				"    ZWrite Off Cull Off Fog { Mode Off } " +
+				"    BindChannels {" +
+				"      Bind \"vertex\", vertex Bind \"color\", color }" +
+				"} } }");
+			lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+			lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+		}
+	}
+
+	void OnPostRender()
+	{
+		CreateLineMaterial();
+		// set the current material
+		lineMaterial.SetPass(0);
+
+		GL.Begin(GL.LINES);
+
+		GL.Color(mainColor);
+
+		DrawPartitioners(PartitionerCenter, CubeWidth);
+		DrawCube(PartitionerCenter, CubeWidth);
+	
+
+		GL.End();
+	}
+}
